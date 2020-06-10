109052dd7feee710ef7c7d8066be4ff6d1af4643
TaiseiSakamoto <falcon_hmktc_9@icloud.com>
2020-06-11 07:24:06 +0900

    Initial Commit

---
+++ MessageApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MessageApp
+//
+//  Created by Taisei Sakamoto on 2020/06/11.
+//  Copyright © 2020 Taisei Sakamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
